68aea75e9ec6dab8d13e0ff9ede95950f704100b
gnocarroll <gocarrol@purdue.edu>
2024-05-05 01:47:30 -0400

    sdl_perror modification

--- gfx_util.h
+++ gfx_util.h
@@ -5,6 +5,6 @@
  *  Some additional utility functions and macros I am writing.
  */
 
-#define sdl_perror(s) fprintf(stderr, "%s:%s\n", s, SDL_GetError())
+#define sdl_perror(s) fprintf(stderr, "%s: %s\n", s, SDL_GetError())
 
 #endif // GFX_UTIL_H
